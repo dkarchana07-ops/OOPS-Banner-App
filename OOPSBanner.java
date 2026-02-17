ddf9b00f46f6b7be69efd2e67ee361e243859d54
dkarchana07-ops <dkarchana07@gmail.com>
2026-02-17 19:58:03 +0530

    Update OOPSBanner.java

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,5 +1,7 @@
 public class OOPSBanner {
     public static void main(String[] args) {
+        String content ="OOPS BANNER APP";
+        
         System.out.println("################################");
         System.out.println("#                              #");
         System.out.println("#       OOPS BANNER APP        #");
