51c2d406ea8224a00e42fd88b8bca53dbfa87e72
dkarchana07-ops <dkarchana07@gmail.com>
2026-02-17 10:30:06 +0530

    UC1: Code using print

---
+++ OOPSBanner.java
@@ -0,0 +1,9 @@
+public class OOPSBanner {
+    public static void main(String[] args) {
+        System.out.println("################################");
+        System.out.println("#                              #");
+        System.out.println("#       OOPS BANNER APP        #");
+        System.out.println("#                              #");
+        System.out.println("################################");
+    }
+}
