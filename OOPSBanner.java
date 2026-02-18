fb2d99c839e0bd6f5c359e8531c0e33f7dab7d27
dkarchana07-ops <dkarchana07@gmail.com>
2026-02-18 17:10:45 +0530

    feat:UC4 refactor complete with arrays and loops

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,9 +1,21 @@
-public class OOPSBanner {
+public class OopsBanner {
     public static void main(String[] args) {
-        System.out.println("################################");
-        System.out.println("#                              #");
-        System.out.println("#       OOPS BANNER APP        #");
-        System.out.println("#                              #");
-        System.out.println("################################");
+        // UC4: Centralized data structure (String Array) to store banner lines
+        // Each line is exactly 26 characters wide to ensure perfect alignment
+        String[] banner = new String[7];
+
+        banner[0] = "#####  #####  #####  #####";
+        banner[1] = "#   #  #   #  #   #  #    ";
+        banner[2] = "#   #  #   #  #   #  #    ";
+        banner[3] = "#   #  #   #  #####  #####";
+        banner[4] = "#   #  #   #  #          #";
+        banner[5] = "#   #  #   #  #          #";
+        banner[6] = "#####  #####  #      #####";
+
+        // UC4: Enhanced for-loop to iterate and print the banner
+        // This eliminates repetitive print statements and improves code modularity
+        for (String line : banner) {
+            System.out.println(line);
+        }
     }
 }
