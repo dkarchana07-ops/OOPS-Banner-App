2e7a3ee51a6360d4e7694d21d4f09c4e3ea8dcee
dkarchana07-ops <dkarchana07@gmail.com>
2026-02-18 15:20:02 +0530

    UC2

--- OOPSBanner.java
+++ OOPSBanner.java
@@ -1,9 +1,12 @@
 public class OOPSBanner {
     public static void main(String[] args) {
-        System.out.println("################################");
-        System.out.println("#                              #");
-        System.out.println("#       OOPS BANNER APP        #");
-        System.out.println("#                              #");
-        System.out.println("################################");
+        // UC2: Large OOPS Banner using 7-line format and 9-character width
+        System.out.println("  ***** " + "   ***** " + "  ***** " + "  ***** ");
+        System.out.println(" *     * " + " *     * " + " *    * " + " *     ");
+        System.out.println(" *     * " + " *     * " + " *    * " + " *      ");
+        System.out.println(" *     * " + " *     * " + " ***** " + "  ***** ");
+        System.out.println(" *     * " + " *     * " + " * " + "          * ");
+        System.out.println(" *     * " + " *     * " + " * " + "          * ");
+        System.out.println("  ***** " + "   *****  "  + " * " + "      ***** ");
     }
 }
